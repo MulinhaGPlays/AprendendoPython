c035db4003846ba33fdd0a3014d8248b51d5962b
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-08 19:52:45 -0300

    implementação de autenticação de usuário

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils import passoword_is_valid
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+            return redirect('/auth/logar')
+        except:
+            return redirect('/auth/cadastro')
+        
+        
+
 def logar(request):
     return HttpResponse("Você está na página de Login")
