94762af9550abf55ba2e568ba6fe1408edc8cba9
MulinhaGPlays <83516034+MulinhaGPlays@users.noreply.github.com>
2022-07-09 17:27:42 -0300

    telas de login e cadastro

--- nutri_lab/urls.py
+++ nutri_lab/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('autenticacao.urls')),
+    path('ativar_conta/<str:token>/', views.ativar_conta, name="ativar_conta"),
 ]
